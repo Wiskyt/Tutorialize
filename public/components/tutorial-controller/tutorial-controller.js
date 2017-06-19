04c8861f7fe61cfe5687ffaafc9d8ad614204ab7
fahemuu <fahem.id@gmail.com>
2017-06-19 17:10:22 +0200

    petit css

--- public/components/tutorial-controller/tutorial-controller.js
+++ public/components/tutorial-controller/tutorial-controller.js
@@ -2,45 +2,58 @@
 
 angular.module('tutorialize')
 
-   .component('tutorialcontroller', {
-      templateUrl: './components/tutorial-controller/tutorial-controller.html',
-      controller: TutorialController
-   })
+    .component('tutorialcontroller', {
+        templateUrl: './components/tutorial-controller/tutorial-controller.html',
+        controller: TutorialController
+    })
 
 function TutorialController($resource) {
-   this.filters = {};
-   this.filtersUser = {};
-   this.tutos = {};
+    this.filters = {};
+    this.filtersUser = {};
+    this.tutos = {};
 
-   // On récupére les tutos
-   requestTutorials(this, $resource);
+    // On récupére les tutos
+    requestTutorials(this, $resource);
+    requestFilters(this , $resource );
 
 
 }
 
 function requestTutorials(self, $resource) {
 
-   var availableTechnosIcons = ["passportjs", "angularjs", "nodejs", "MEAN"]; // Array that lists all icons repertoried
+    var availableTechnosIcons = ["passportjs", "angularjs", "nodejs", "MEAN"]; // Array that lists all icons repertoried
 
-   var request = $resource("/data");
-   request.get().$promise.then((data) => {
-      let tutorials = data.data;
+    var request = $resource("/data");
+    request.get().$promise.then((data) => {
+        let tutorials = data.data;
 
-      tutorials.map((e) => {
-         let t = e.techno.split(", "), technos = [];
-         for (let i = 0; i < t.length; i++) {
-            let obj = {
-               name: t[i],
-               hasImg: availableTechnosIcons.indexOf(t[i]) > -1
+        tutorials.map((e) => {
+            let t = e.techno.split(", "),
+                technos = [];
+            for (let i = 0; i < t.length; i++) {
+                let obj = {
+                    name: t[i],
+                    hasImg: availableTechnosIcons.indexOf(t[i]) > -1
+                }
+                technos.push(obj);
+                //  technos.push(obj); // TODO: Flexbox layout for 2+ icons
             }
-            technos.push(obj);
-            //  technos.push(obj); // TODO: Flexbox layout for 2+ icons
-         }
 
-         e.techno = technos;
-      });
+            e.techno = technos;
+        });
 
-      self.tutos = tutorials.concat(tutorials);
-      console.log(self.tutos);
-   })
+        self.tutos = tutorials.concat(tutorials);
+        console.log(self.tutos);
+    })
+
+
+}
+function requestFilters(self, $resource) {
+    var resFilters = $resource("/filters");
+
+    resFilters.get().$promise.then((filters) => {
+        self.filters = filters;
+
+        console.log("request filters",filters);
+    })
 }
